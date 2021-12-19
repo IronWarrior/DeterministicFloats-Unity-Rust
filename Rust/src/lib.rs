457f2f712b2edfc367e3d1f614c63dad03da46b1
Erik <erikroystan@gmail.com>
2021-12-19 00:04:27 -0800

    Switch to transmute to convert types, add method attributes to prevent compiler modifications.

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -1,23 +1,33 @@
 #[no_mangle]
-pub extern fn float_add(a: u32, b: u32) -> u32 {	
-    let result : f32 = f32::from_bits(a) + f32::from_bits(b);
-	return result.to_bits();
+pub unsafe extern fn float_add(a: u32, b: u32) -> u32 {	
+    let result : f32 = from_bits(a) + from_bits(b);
+	return to_bits(result);
 }
 
 #[no_mangle]
-pub extern fn float_sub(a: u32, b: u32) -> u32 {	
-    let result : f32 = f32::from_bits(a) - f32::from_bits(b);
-	return result.to_bits();
+pub unsafe extern fn float_sub(a: u32, b: u32) -> u32 {	
+    let result : f32 = from_bits(a) - from_bits(b);
+	return to_bits(result);
 }
 
 #[no_mangle]
-pub extern fn float_mul(a: u32, b: u32) -> u32 {	
-    let result : f32 = f32::from_bits(a) * f32::from_bits(b);
-	return result.to_bits();
+pub unsafe extern fn float_mul(a: u32, b: u32) -> u32 {	
+    let result : f32 = from_bits(a) * from_bits(b);
+	return to_bits(result);
 }
 
 #[no_mangle]
-pub extern fn float_div(a: u32, b: u32) -> u32 {	
-    let result : f32 = f32::from_bits(a) / f32::from_bits(b);
-	return result.to_bits();
+pub unsafe extern fn float_div(a: u32, b: u32) -> u32 {	
+    let result : f32 = from_bits(a) / from_bits(b);
+	return to_bits(result);
+}
+
+#[no_mangle]
+unsafe fn to_bits(f: f32) -> u32 {
+	return std::mem::transmute::<f32, u32>(f);
+}
+
+#[no_mangle]
+unsafe fn from_bits(bits: u32) -> f32 {
+	return std::mem::transmute::<u32, f32>(bits);
 }
--- Unity/Assets/Mathd.cs
+++ Unity/Assets/Mathd.cs
@@ -1,40 +1,42 @@
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 public static class Mathd
 {
-    [DllImport("unity_rust")]
-    private static extern uint float_add(uint a, uint b);
+    //[DllImport("unity_rust")]
+    //private static extern uint float_add(uint a, uint b);
 
-    [DllImport("unity_rust")]
-    private static extern uint float_sub(uint a, uint b);
+    //[DllImport("unity_rust")]
+    //private static extern uint float_sub(uint a, uint b);
 
     [DllImport("unity_rust")]
     private static extern uint float_mul(uint a, uint b);
 
-    [DllImport("unity_rust")]
-    private static extern uint float_div(uint a, uint b);
+    //[DllImport("unity_rust")]
+    //private static extern uint float_div(uint a, uint b);
 
-    public static dfloat Add(dfloat a, dfloat b)
-    {
-        uint bits = float_add(a.Bits, b.Bits);
-        return new dfloat(bits);
-    }
+    //public static dfloat Add(dfloat a, dfloat b)
+    //{
+    //    uint bits = float_add(a.Bits, b.Bits);
+    //    return new dfloat(bits);
+    //}
 
-    public static dfloat Sub(dfloat a, dfloat b)
-    {
-        uint bits = float_sub(a.Bits, b.Bits);
-        return new dfloat(bits);
-    }
+    //public static dfloat Sub(dfloat a, dfloat b)
+    //{
+    //    uint bits = float_sub(a.Bits, b.Bits);
+    //    return new dfloat(bits);
+    //}
 
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization | MethodImplOptions.PreserveSig)]
     public static dfloat Mul(dfloat a, dfloat b)
     {
         uint bits = float_mul(a.Bits, b.Bits);
         return new dfloat(bits);
     }
 
-    public static dfloat Div(dfloat a, dfloat b)
-    {
-        uint bits = float_div(a.Bits, b.Bits);
-        return new dfloat(bits);
-    }
+    //public static dfloat Div(dfloat a, dfloat b)
+    //{
+    //    uint bits = float_div(a.Bits, b.Bits);
+    //    return new dfloat(bits);
+    //}
 }
